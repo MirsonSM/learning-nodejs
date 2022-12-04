8f93e1a1d5b9f1170e1f50bc33f34558c5512a08
Mailson Moreira <67443036+MirsonSM@users.noreply.github.com>
2022-12-03 21:25:05 -0300

    implementação de subtitle e items

--- server.js
+++ server.js
@@ -4,7 +4,34 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", function(req, res){
-    res.render("pages/index")
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver aplicações/serviçoes de forma fácil",
+        },
+        {
+            title: "E",
+            message: "JS usa JavaScripit para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito Fácil de Usar"
+        },
+        {
+            title: "A",
+            message: "gil",
+        },
+        {
+            title: "i",
+            message: "nstall EJS",
+        },
+        {
+            title: "S",
+            message: "imples",
+        },
+    ];
+    const subtitle = 'Uma Linguagem de modelagem para criação de página HTML'
+    res.render("pages/index", {qualitys: items, subtitle: subtitle})
 })
 
 app.get("/sobre", function(req, res){
